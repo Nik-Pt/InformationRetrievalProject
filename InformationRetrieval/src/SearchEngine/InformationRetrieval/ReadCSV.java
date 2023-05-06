08c26f730ce43e41dd7efc88fd07e267cf922a49
Nik-Pt <nikpit2002@gmail.com>
2023-05-06 21:59:51 +0300

    Version 0.7

---
+++ InformationRetrieval/src/SearchEngine/InformationRetrieval/ReadCSV.java
@@ -0,0 +1,48 @@
+package  SearchEngine.InformationRetrieval;
+
+import java.io.*;
+import java.util.*;
+
+public class ReadCSV{
+
+    private String folderPath = "csv";
+    private File folder = new File(folderPath);
+    private File[] fileList = folder.listFiles();
+
+    public  ArrayList<Song> toSongList(){
+        ArrayList<Song> songList = new ArrayList<Song>();
+        if(!folder.exists()){
+            System.out.println("Error: directory does not exist: " + folderPath);
+            System.exit(0);
+        }
+        if(!folder.isDirectory()){
+            System.out.println("Error: not a directory: " + folderPath);
+            System.exit(0);
+        }
+
+        for(File file : fileList){
+            if(file.isFile() && file.getName().endsWith(".csv")){
+                try{
+                    BufferedReader csvReader = new BufferedReader(new FileReader(file));
+                    String line;
+                    csvReader.readLine(); //Skips the first line because of the header in each .csv file
+                    while((line = csvReader.readLine()) != null){
+                        String[] data = line.split(",");
+                        String artist = data[1];
+                        String title = data[2];
+                        String album = data[3];
+                        String year = data[4];
+                        String date = data[5];
+                        String lyrics = data[6];
+                        Song song = new Song(artist,title,album,date,lyrics,year);
+                        songList.add(song);
+                    }
+                    csvReader.close();
+                }catch(IOException exception){
+                    exception.printStackTrace();
+                }
+            }
+        }
+        return songList;
+    }
+}
---
+++ InformationRetrieval/src/SearchEngine/InformationRetrieval/Song.java
@@ -0,0 +1,54 @@
+package SearchEngine.InformationRetrieval;
+
+public class Song {
+    private String artist;
+    private String title;
+    private String album;
+    private String date;
+    private String lyrics;
+    private String year;
+
+    public Song(String artist,String title,String album,String date,String lyrics,String year){
+        this.artist = artist;
+        this.title = title;
+        this.album = album;
+        this.date = date;
+        this.lyrics = lyrics;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return "Song{" +
+                "artist='" + artist + '\'' +
+                ", title='" + title + '\'' +
+                ", album='" + album + '\'' +
+                ", date='" + date + '\'' +
+                ", lyrics='" + lyrics + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+}
